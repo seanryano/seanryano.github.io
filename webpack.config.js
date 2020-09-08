540eafe50c667e949fc8d5d3209ff05b3370024b
seanryano <seangerard.ryan@gonitro.com>
2020-09-08 13:53:48 +0100

    Initial Commit
    This is first commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+            options: {
+              // Include the path to Vanilla
+              includePaths: ['./node_modules']
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
